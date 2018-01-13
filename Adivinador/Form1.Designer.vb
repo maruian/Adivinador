5a4f508674bb7706c0a4836550043a1f1a39b655
Maties <maruian@floridauniversitaria.es>
2018-01-13 21:23:36 +0100

    Adivinador magico

---
+++ Adivinador/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAleatorio = New System.Windows.Forms.Button()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbTecleados = New System.Windows.Forms.ListBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAleatorio
+        '
+        Me.btnAleatorio.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAleatorio.Location = New System.Drawing.Point(12, 13)
+        Me.btnAleatorio.Name = "btnAleatorio"
+        Me.btnAleatorio.Size = New System.Drawing.Size(321, 55)
+        Me.btnAleatorio.TabIndex = 0
+        Me.btnAleatorio.Text = "Obtener un número entre 1 y 100"
+        Me.btnAleatorio.UseVisualStyleBackColor = True
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Enabled = False
+        Me.txtNumero.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumero.Location = New System.Drawing.Point(163, 108)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(170, 26)
+        Me.txtNumero.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(144, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Teclea un número: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 160)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Números tecleados:"
+        '
+        'lbTecleados
+        '
+        Me.lbTecleados.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbTecleados.FormattingEnabled = True
+        Me.lbTecleados.ItemHeight = 20
+        Me.lbTecleados.Location = New System.Drawing.Point(17, 184)
+        Me.lbTecleados.Name = "lbTecleados"
+        Me.lbTecleados.Size = New System.Drawing.Size(316, 104)
+        Me.lbTecleados.TabIndex = 4
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(34, 308)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(140, 55)
+        Me.btnLimpiar.TabIndex = 5
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(180, 308)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(140, 55)
+        Me.btnSalir.TabIndex = 6
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 390)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.lbTecleados)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.btnAleatorio)
+        Me.Name = "Form1"
+        Me.Text = "Juego de encontrar un número"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAleatorio As Button
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lbTecleados As ListBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ Adivinador/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+
+    Dim aleatorio As Integer
+    Dim contador As Integer
+
+
+    Private Sub btnAleatorio_Click(sender As Object, e As EventArgs) Handles btnAleatorio.Click
+        Randomize()
+        ' El valor aleatorio debe estar entre uno y cien
+        ' N = (Limsup - Liminf) * Rnd + Liminf
+        aleatorio = CInt(Rnd() * 99) + 1
+        contador = 1
+        btnAleatorio.Enabled = False
+        txtNumero.Enabled = True
+    End Sub
+
+
+    Private Sub txtNumero_KeyPress(sender As Object, e As KeyEventArgs) Handles txtNumero.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If IsNumeric(txtNumero.Text) Then
+                If CInt(txtNumero.Text) > aleatorio Then
+                    lbTecleados.Items.Add(txtNumero.Text & " Numero demasiado grande")
+                    lbTecleados.TopIndex = lbTecleados.Items.Count - 1
+                    contador = contador + 1
+                ElseIf CInt(txtNumero.Text) < aleatorio Then
+                    lbTecleados.Items.Add(txtNumero.Text & " Numero demasiado pequeño")
+                    lbTecleados.TopIndex = lbTecleados.Items.Count - 1
+                    contador = contador + 1
+                Else
+                    lbTecleados.Items.Add(txtNumero.Text & " FELICIDADES. Lo has encontrado")
+                    lbTecleados.Items.Add("Intentos empleados: " & contador)
+                    lbTecleados.TopIndex = lbTecleados.Items.Count - 1
+                    txtNumero.Enabled = False
+                End If
+            Else
+                MsgBox("Debes introducir un número!")
+            End If
+            txtNumero.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        btnAleatorio.Enabled = True
+        txtNumero.Enabled = False
+        lbTecleados.Items.Clear()
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+    End Sub
+
+End Class
